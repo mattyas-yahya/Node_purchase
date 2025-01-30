e474c65e1bcc3aff3e872d57106bc10724ff491b
matt_ <rahmadandmysella@gmail.com>
2025-01-30 09:47:42 +0700

    Update database.js
    update localhost ke https://node-purchase.vercel.app/

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const Sequelize  = require('sequelize');
 
 const sequelize = new Sequelize('node_manufacture', 'root', 'root', {
-  host: 'localhost',
+  host: 'https://node-purchase.vercel.app',
   dialect: 'mysql',
 });
 
