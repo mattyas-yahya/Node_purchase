1c7eac7452e20c57358020ce481f5f142c95586e
matt_ <rahmadandmysella@gmail.com>
2025-01-30 10:04:51 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,18 @@
-const Sequelize  = require('sequelize');
+const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('node_manufacture', 'root', 'root', {
-  host: 'https://node-purchase.vercel.app',
-  dialect: 'mysql',
+const sequelize = new Sequelize(process.env.POSTGRES_DATABASE, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+  host: process.env.POSTGRES_HOST,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
+
+sequelize.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch(err => console.error('Database connection error:', err));
 
 module.exports = sequelize;
