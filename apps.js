99516b7bbbd87b4f18f228030f06c2137f05ef7c
matt_ <rahmadandmysella@gmail.com>
2025-02-07 10:27:39 +0700

    Update apps.js
    update port ke process.env.PORT

--- apps.js
+++ apps.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+// const port = 3000;
+const port = process.env.PORT;
 const path = require('path');
 const cors = require('cors');
 app.use(cors());
